268b0b2a65cbf8c2747a04e3cfefb0dc24b2a073
discopauk <diskopauk@yandex.ru>
2021-02-05 14:53:37 +0300

    add modal + submenu

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,1 +1,13 @@
-console.log("works");
+const toggle = document.querySelector(".toggle");
+const body = document.querySelector("body");
+const calc = document.querySelector(".calc");
+const close = document.querySelector(".close");
+toggle.addEventListener("click", () => {
+  body.classList.toggle("openHamb");
+})
+calc.addEventListener("click", () => {
+  body.classList.add("openModal");
+})
+close.addEventListener("click", () => {
+  body.classList.remove("openModal");
+})
