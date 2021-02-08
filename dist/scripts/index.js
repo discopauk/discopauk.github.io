b8943c6fd64ebba8dcf2ae39ede8be7451d16883
discopauk <diskopauk@yandex.ru>
2021-02-08 12:12:40 +0300

    cleared js

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,9 +1,10 @@
+"use strict"
+
 const toggle = document.querySelector(".toggle");
 const body = document.querySelector("body");
 const calc = document.querySelector(".calc");
 const close = document.querySelector(".close");
-const next = document.querySelector(".next");
-const prev = document.querySelector(".prev");
+
 toggle.addEventListener("click", () => {
   body.classList.toggle("openHamb");
 })
@@ -12,35 +13,4 @@ calc.addEventListener("click", () => {
 })
 close.addEventListener("click", () => {
   body.classList.remove("openModal");
-})
-const first = document.querySelector(".field:nth-of-type(1)")
-const second = document.querySelector(".field:nth-of-type(2)");
-const third = document.querySelector(".field:nth-of-type(3)");
-const step = document.querySelector(".step");
-next.addEventListener("click", () => {
-  if (second.hasAttribute("style", "display:none")) {
-    second.setAttribute("style", "display:flex");
-    first.setAttribute("style", "display:none");
-    step.textContent = "Шаг 2 из 3";
-    prev.removeAttribute("disabled", "disabled");
-  } else {
-    third.setAttribute("style", "display:flex");
-    second.setAttribute("style", "display:none");
-    step.textContent = "Шаг 3 из 3";
-    next.setAttribute("disabled", "disabled");
-  }
-})
-prev.addEventListener("click", () => {
-  if (first.hasAttribute("style", "display:none")) {
-    first.setAttribute("style", "display:flex");
-    second.setAttribute("style", "display:none");
-    step.textContent = "Шаг 1 из 3";
-    prev.setAttribute("disabled", "disabled");
-  } else {
-    third.setAttribute("style", "display:none");
-    first.setAttribute("style", "display:none");
-    second.setAttribute("style", "display:flex");
-    step.textContent = "Шаг 2 из 3";
-    next.removeAttribute("disabled", "disabled")
-  }
 })
---
+++ dist/scripts/modal.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const step = document.querySelector(".step");
+const field = document.querySelectorAll(".field");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+next.addEventListener("click", () => {
+  if (field[0].hasAttribute("style", "display:flex")) {
+    field[0].removeAttribute("style");
+    field[1].setAttribute("style", "display:flex");
+    prev.removeAttribute("disabled", "disabled");
+    step.textContent = "Шаг 2 из 3";
+  } else {
+    field[1].removeAttribute("style", "display:flex");
+    field[2].setAttribute("style", "display:flex");
+    next.setAttribute("disabled", "disabled");
+    step.textContent = "Шаг 3 из 3";
+  }
+})
+prev.addEventListener("click", () => {
+  if (field[2].hasAttribute("style", "display:flex")) {
+    field[2].removeAttribute("style", "display:flex");
+    field[1].setAttribute("style", "display:flex");
+    next.removeAttribute("disabled", "disabled");
+    step.textContent = "Шаг 2 из 3";
+  } else {
+    field[1].removeAttribute("style", "display:flex");
+    field[0].setAttribute("style", "display:flex");
+    prev.setAttribute("disabled", "disabled");
+    step.textContent = "Шаг 1 из 3";
+  }
+})
