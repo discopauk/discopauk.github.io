44049d55f8985e3fe6999574eaa65f5c958765db
discopauk <diskopauk@yandex.ru>
2021-02-08 18:13:46 +0300

    added slideshow

--- dist/scripts/modal.js
+++ dist/scripts/modal.js
@@ -4,27 +4,27 @@ const step = document.querySelector(".step");
 const field = document.querySelectorAll(".field");
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
-next.addEventListener("click", () => {
+next.addEventListener("click", function() {
   if (field[0].hasAttribute("style", "display:flex")) {
     field[0].removeAttribute("style");
     field[1].setAttribute("style", "display:flex");
-    prev.removeAttribute("disabled", "disabled");
+    prev.removeAttribute("disabled");
     step.textContent = "Шаг 2 из 3";
   } else {
-    field[1].removeAttribute("style", "display:flex");
+    field[1].removeAttribute("style");
     field[2].setAttribute("style", "display:flex");
     next.setAttribute("disabled", "disabled");
     step.textContent = "Шаг 3 из 3";
   }
 })
-prev.addEventListener("click", () => {
+prev.addEventListener("click", function() {
   if (field[2].hasAttribute("style", "display:flex")) {
-    field[2].removeAttribute("style", "display:flex");
+    field[2].removeAttribute("style");
     field[1].setAttribute("style", "display:flex");
-    next.removeAttribute("disabled", "disabled");
+    next.removeAttribute("disabled");
     step.textContent = "Шаг 2 из 3";
   } else {
-    field[1].removeAttribute("style", "display:flex");
+    field[1].removeAttribute("style");
     field[0].setAttribute("style", "display:flex");
     prev.setAttribute("disabled", "disabled");
     step.textContent = "Шаг 1 из 3";
